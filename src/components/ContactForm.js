1036835f9db28da73c8ed572ee5a8e9524450148
dialguiba <diego.aguirre@unmsm.edu.pe>
2020-12-03 01:08:28 -0500

    fixingcontact4

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,17 +8,17 @@ function Contactform() {
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
-            Your Name: <input type="text" name="name" />
+            <input type="text" name="name" placeholder="Your name" required />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type="email" name="email" />
+            <input type="email" name="email" placeholder="Your Email" required />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message" />
+            <textarea name="message" placeholder="Message" required />
           </label>
         </p>
         <p>
